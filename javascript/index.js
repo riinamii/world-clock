a354b1f6dd97ba9f30091038ac10f8321e4c93db
riinamii <179370603+riinamii@users.noreply.github.com>
2025-10-18 16:09:39 +0300

    Simplify time update logic using dataset and forEach

--- javascript/index.js
+++ javascript/index.js
@@ -1,42 +1,17 @@
 function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+  let cities = document.querySelectorAll(".city");
+  if (cities) {
+    cities.forEach((city) => {
+      let cityTimeZone = city.dataset.timezone;
+      let date = moment().tz(cityTimeZone).format("MMMM Do YYYY");
+      let time = moment().tz(cityTimeZone).format("h:mm:ss A");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss A");
-  }
+      let updateTime = city.querySelector(".time");
+      let updateDate = city.querySelector(".date");
 
-  let lisbonElement = document.querySelector("#lisbon");
-  if (lisbonElement) {
-    let lisbonDateElement = lisbonElement.querySelector(".date");
-    let lisbonTimeElement = lisbonElement.querySelector(".time");
-    let lisbonTime = moment().tz("Europe/Lisbon");
-
-    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-    lisbonTimeElement.innerHTML = lisbonTime.format("h:mm:ss A");
-  }
-
-  let kyivElement = document.querySelector("#kyiv");
-  if (kyivElement) {
-    let kyivDateElement = kyivElement.querySelector(".date");
-    let kyivTimeElement = kyivElement.querySelector(".time");
-    let kyivTime = moment().tz("Europe/Kyiv");
-
-    kyivDateElement.innerHTML = kyivTime.format("MMMM Do YYYY");
-    kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss A");
-  }
-
-  let tokyoElement = document.querySelector("#tokyo");
-  if (tokyoElement) {
-    let tokyoDateElement = tokyoElement.querySelector(".date");
-    let tokyoTimeElement = tokyoElement.querySelector(".time");
-    let tokyoTime = moment().tz("Asia/Tokyo");
-
-    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
+      updateTime.innerHTML = time;
+      updateDate.innerHTML = date;
+    });
   }
 }
 
@@ -48,14 +23,17 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-    <div class="city">
+  citiesElement.innerHTML = `  
+  <div class="selected-city">
     <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-        <div class="time">${cityTime.format("h:mm:ss A")}</div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
     </div>
+    <a href="/" class="all-cities-btn">All cities</a>
     `;
 }
 
